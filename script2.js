c09684555b8ac81ef3efe7e2ed3b1979e08bc76a
Bo Gustavsson <bosseg09@gmail.com>
2023-09-03 15:27:58 +0200

    bosse concept favourites

---
+++ script2.js
@@ -0,0 +1,51 @@
+document.getElementById("favouriteStatus").textContent = "Dina favoriter:";
+
+
+function makeFavourite(className) {
+    var favouriteStatus = document.getElementById("favouriteStatus");
+    var favOne = document.getElementById("favOne");
+    var favTwo = document.getElementById("favTwo");
+    var favThree = document.getElementById("favThree");
+    var favFour = document.getElementById("favFour");
+    var favFive = document.getElementById("favFive");
+
+    var classDiv = document.getElementsByClassName(className)[0];
+    var starImg = classDiv.getElementsByTagName("img")[0];
+
+    if (starImg.src.includes("star2.png")) {
+      // Lägg till som favorit
+      if (favOne.innerHTML === "") {
+        favOne.innerHTML = className;
+      } else if (favTwo.innerHTML === "") {
+        favTwo.innerHTML = className;
+      } else if (favThree.innerHTML === "") {
+        favThree.innerHTML = className;
+      } else if (favFour.innerHTML === "") {
+        favFour.innerHTML = className;
+      } else if (favFive.innerHTML === "") {
+        favFive.innerHTML = className;
+      } else {
+        favouriteStatus.innerHTML = "Du har redan fem favoriter!";
+        return;
+      }
+
+      starImg.src = "imgs/star1.png";
+    } else {
+      // Ta bort som favorit
+      if (favOne.innerHTML === className) {
+        favOne.innerHTML = "";
+      } else if (favTwo.innerHTML === className) {
+        favTwo.innerHTML = "";
+      } else if (favThree.innerHTML === className) {
+        favThree.innerHTML = "";
+      } else if (favFour.innerHTML === className) {
+        favFour.innerHTML = "";
+      } else if (favFive.innerHTML === className) {
+        favFive.innerHTML = "";
+      }
+
+      starImg.src = "imgs/star2.png";
+    }
+
+    favouriteStatus.innerHTML = "Dina favoriter:";
+  }
