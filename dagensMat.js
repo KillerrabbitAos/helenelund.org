d341e2f4c6752c0e61d0141d13f071f8840c8536
Bo Gustavsson <bosseg09@gmail.com>
2024-08-22 20:56:09 +0200

    mat mmm

---
+++ dagensMat.js
@@ -0,0 +1,37 @@
+async function fetchMenu() {
+    try {
+        const response = await fetch('http://79.72.19.53:3000/menu');
+        const data = await response.json();
+        renderTodaysMenu(data);
+    } catch (error) {
+        console.error('Fel vid hämtning av meny:', error);
+    }
+}
+
+function renderTodaysMenu(menuData) {
+    const matDiv = document.getElementById('mat');
+    matDiv.innerHTML = '';
+
+    if (menuData.weeks && menuData.weeks.length > 0) {
+        const today = new Date();
+
+        const todaysMenu = menuData.weeks[0].days.find(day => {
+            const dayDate = new Date(day.date * 1000);
+            return dayDate.toDateString() === today.toDateString();
+        });
+
+        if (todaysMenu) {
+            const formattedDate = today.toLocaleDateString('sv-SE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+            const vegetarisk = `<span class="vegetarisk">(Vegetarisk) ${todaysMenu.items[0]}</span>`;
+            const kött = `<span class="kott">(Kött) ${todaysMenu.items.slice(1).join(', ')}</span>`;
+
+            matDiv.innerHTML = `<strong>${formattedDate}:</strong><br>${kött}<br>${vegetarisk}`;
+        } else {
+            matDiv.innerHTML = '<p>Ingen matinformation tillgänglig för idag.</p>';
+        }
+    } else {
+        matDiv.innerHTML = '<p>Ingen meny tillgänglig</p>';
+    }
+}
+
+window.onload = fetchMenu;
---
+++ veckansmat/script.js
@@ -0,0 +1,35 @@
+async function fetchMenu() {
+    try {
+        const response = await fetch('http://79.72.19.53:3000/menu');
+        const data = await response.json();
+        renderMenu(data);
+    } catch (error) {
+        console.error('Tekniska problem..:', error);
+    }
+}
+
+function renderMenu(menuData) {
+    const menuList = document.getElementById('menu-list');
+    menuList.innerHTML = '';
+
+    if (menuData.weeks && menuData.weeks.length > 0) {
+        const today = new Date();
+
+        menuData.weeks[0].days.forEach(day => {
+            const li = document.createElement('li');
+            const date = new Date(day.date * 1000);
+            const formattedDate = date.toLocaleDateString('sv-SE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+            if (date.toDateString() === today.toDateString()) {
+                li.classList.add('highlighted');
+            }
+
+            li.innerHTML = `<strong><span class="datum">${formattedDate}:</span></strong><br>${day.items.join(',<br>')}`;
+            menuList.appendChild(li);
+        });
+    } else {
+        menuList.innerHTML = '<li>Ingen meny tillgänglig</li>';
+    }
+}
+
+window.onload = fetchMenu;
